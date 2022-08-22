18e4b3f40c25d09b8c14530a3f46a2406ff91412
aj1143208 <aj1143208@gmail.com>
2022-08-22 17:15:18 +0800

    Update config.js

--- config.js
+++ config.js
@@ -16,6 +16,7 @@ var config = {
         "所以",
         "一切随缘而定即可",
         "YAMI~~",
+        "see you for the first time",
     ],
     /**
      * imgs 可以不填, 但是如果要填写的话必须遵循下面的格式
@@ -27,20 +28,20 @@ var config = {
      * Tip: 图片最好用正方形or接近正方形, 看起来效果更好
      */
     imgs: {
-        "see you for the first time": "./imgs/xiaokeai.png",
+        "see you for the first time": "./imgs/wenqing.png",
         
            //         "心爱的小可爱": "./imgs/xiaokeai.png",
         // "今天是你的生日": "./imgs/birthday.jpg",
     },
     // 按钮文字描述, 以下是默认的按钮文字，英文的，您可以改成你喜欢的文字
     desc: {
-        turn_on: "点我开始",
-        play: "音乐滴要的",
-        bannar_coming: "换个颜色",
-        balloons_flying: "呀，好像少点东西",
+        turn_on: "点我点我点我",
+        play: "音乐滴要的要的",
+        bannar_coming: "换个颜色试试",
+        balloons_flying: "呀，好像少了点东西",
         cake_fadein: "蛋糕？",
         light_candle: "蜡烛？",
-        wish_message: "生日快乐",
+        wish_message: "是生日快乐！！！",
         story: "A MESSAGE FOR YOU",
     }
 };
